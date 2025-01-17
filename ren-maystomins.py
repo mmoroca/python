aa964d5af495e26394a815cc60c4ac4b2c55611b
mmoroca <mmoroca@gmail.com>
2025-01-17 17:51:53 +0100

    Update ren-maystomins.py

--- ren-maystomins.py
+++ ren-maystomins.py
@@ -8,7 +8,7 @@ def rename_files_to_lowercase(directory):
             os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
 
 if __name__ == "__main__":
-    directory = sys.argv[1] if len(sys.argv) > 1 else "."  # current directory if no argument is provided
-    print("Renaming files in directory", directory)
+    directory = sys.argv[1] if len(sys.argv) > 1 else "."  # si no se pasa directorio toma el actual
+    print("Renombrando archivos en el directorio", directory)
     rename_files_to_lowercase(directory)
-    print("Done!")
+    print("Hecho.")
